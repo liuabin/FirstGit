ac2820b5ea226ca4852c59e2a21879b1e2c04563
liuabin <hzlxb123@gmail.com>
2018-02-11 21:47:55 +0800

    First Project in Git.

---
+++ Hello,Git.cpp
@@ -0,0 +1,14 @@
+/*
+ * First Cpp in Git.
+ * Liu Xiangbin
+ * 18.2.11 21:43
+ */
+#include<iostream>
+#include<windows.h>
+
+int main() {
+	std::cout<<"Hello, Git! "<<std::endl;
+
+	system("pause");
+	return 0;
+}
